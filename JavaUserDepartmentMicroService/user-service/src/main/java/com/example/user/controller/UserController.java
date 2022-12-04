d7987e803de0210e85fd3eb7038af81177b3a0a4
pankajproject99 <pankajproject99@gmail.com>
2022-12-05 01:06:18 +0530

    JavaUserDepartmentMicroService : zipkin installed but logs are not showing service, tag etc

--- JavaUserDepartmentMicroService/user-service/src/main/java/com/example/user/controller/UserController.java
+++ JavaUserDepartmentMicroService/user-service/src/main/java/com/example/user/controller/UserController.java
@@ -4,13 +4,16 @@ import com.example.user.VO.ResponseTemplateVO;
 import com.example.user.entity.User;
 import com.example.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Slf4j
 @RequestMapping("/users")
 public class UserController {
+
+    private static Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService userService;
